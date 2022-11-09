70e1fdc641cbabf82272d66f095aebbfeff8dea1
Vasilevich Dmitry <vasilevich.dmt@gmail.com>
2022-11-09 18:33:50 +0300

    Change background image for NotFound page

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -4,7 +4,10 @@ import styles from "./NotFound.module.css";
 
 export const NotFound: FC = () => {
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      style={{ backgroundImage: "url(/bg-404.jpg)" }}
+    >
       <span className={styles.text}>404</span>
       <span className={styles.text}>Content not found</span>
     </div>
